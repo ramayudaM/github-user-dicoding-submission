53b75a56a8abab33d5e363e6ea7ac4f40f8daef8
ramayudaM <ramayudaid@gmail.com>
2022-09-27 11:16:22 +0700

    submission 1

--- app/src/main/java/com/ramayuda/githubuser/SplashScreenActivity.kt
+++ app/src/main/java/com/ramayuda/githubuser/SplashScreenActivity.kt
@@ -7,8 +7,10 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 
+
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -16,7 +18,11 @@ class SplashScreenActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 2000)
+        }, TIME_SPLASH_SCREEN)
 
+    }
+
+    companion object {
+        const val TIME_SPLASH_SCREEN = 2000L
     }
 }
